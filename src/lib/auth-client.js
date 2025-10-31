7df575814df259b1336ea47ef64f511ed9e571a4
Ronald Otieno <ronnlats@gmail.com>
2025-10-31 17:50:58 +0300

    fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,11 +3,11 @@
  * Uses Better-Auth Svelte integration
  */
 
-import { env } from "$env/dynamic/private"; // ✅ dynamic import (safe)
+import { BETTER_AUTH_URL } from "$env/static/private";
 import { createAuthClient } from "better-auth/svelte";
 
 export const authClient = createAuthClient({
-  baseURL:env.BETTER_AUTH_URL || "http://localhost:5173", // Will use same domain in production
+  baseURL: BETTER_AUTH_URL || "http://localhost:5173", // Will use same domain in production
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
